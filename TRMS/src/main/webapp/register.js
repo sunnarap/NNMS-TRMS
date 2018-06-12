fa80945fadae158a614bbe3ffb6e4a3213bc2f99
sunnarap <sunnaraprasavath@gmail.com>
2018-06-11 23:07:59 -0400

    Sorry accidently deleted everything

---
+++ TRMS/src/main/webapp/register.js
@@ -0,0 +1,25 @@
+var fname = document.getElementById("fname");
+var lname = document.getElementById("lname");
+var user = document.getElementById("email");
+var pw = document.getElementById("password");
+var verify = document.getElementById("password2");
+var buttons = document.getElementById("submituser");
+var form = document.getElementById("registerdata");
+
+
+function getInfo() {
+    form = document.getElementById("registerdata");
+    var xhr = new XMLHttpRequest();
+    var forminfo = new FormData(form);
+    xhr.open("POST","\loginscreen",true);
+    xhr.send(forminfo);
+}
+
+
+window.onload = function() {
+    buttons.addEventListener("click",function() {
+    	
+    });
+    form.addEventListener("submit",getInfo);
+
+}
